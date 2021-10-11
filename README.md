4408ed6f5efae02601947b61e731683455f06b95
zulhafiz-zulkifli <zulh2605@gmail.com>
2021-10-11 23:59:32 +0800

    update diff_tf

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# medibotv4_ros_ //
---> ROS development ----->
+# medibotv4_ros
+
+
+TODO LIST:
+- [ ] Arduino encoder library
+- [ ] Closed-loop motor control
+- [ ] Encoder odometry correction
+- [ ] Udev rules
+- [ ] Arduino libraries from local subfolder
+- [ ] Installation scripts
+- [ ] robot_upstart
--- medibotv4/src/encoder_ticks_publisher.py
+++ medibotv4/src/encoder_ticks_publisher.py
@@ -3,8 +3,8 @@ import rospy
 from std_msgs.msg import Int16
 
 def talker():
-    publ = rospy.Publisher('lwheel', Int16, queue_size=10)
-    pubr = rospy.Publisher('rwheel', Int16, queue_size=10)
+    publ = rospy.Publisher('lwheel_ticks', Int16, queue_size=10)
+    pubr = rospy.Publisher('rwheel_ticks', Int16, queue_size=10)
     rospy.init_node('encoder_publisher_node', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     msgl = Int16()
@@ -17,7 +17,7 @@ def talker():
         pubr.publish(msgr)
         rate.sleep()
     	msgl.data+=1
-        msgr.data+=1
+        msgr.data+=-1
 
 if __name__ == '__main__':
     try:
