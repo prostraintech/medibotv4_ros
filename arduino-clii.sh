9daf1e29cf3061c1bbd7d9f90b80eeb65028e67d
zulhafiz-zulkifli <zulh2605@gmail.com>
2021-12-24 02:19:16 +0800

    update arduino cli

--- arduino-clii.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git pull
-./arduino-cli compile --fqbn arduino:avr:uno --libraries /arduino/main/libraries/Arduino_PID_Library,/arduino/main/libraries/Rosserial_Arduino_Library arduino/main
-./arduino-cli upload  --fqbn arduino:avr:uno --port /dev/ttyACM0 arduino/main
