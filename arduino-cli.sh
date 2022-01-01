00eca36c2ad41c2f9ee93b9776f47cda929ac1aa
zulhafiz-zulkifli <zulh2605@gmail.com>
2022-01-01 22:17:31 +0800

    test

--- arduino-cli.sh
+++ arduino-cli.sh
@@ -2,5 +2,3 @@
 
 ./arduino-cli compile --fqbn per1234:sam:arduino_due_x_dbg arduino/main
 ./arduino-cli upload  --fqbn per1234:sam:arduino_due_x_dbg --port /dev/ttyACM0 arduino/main
-
-#./arduino-cli compile --fqbn per1234:sam:arduino_due_x_dbg --libraries /arduino/main/libraries/Arduino_PID_Library,/arduino/main/libraries/Rosserial_Arduino_Library arduino/main
