20c281aa4fe68ea5f416963377f9e3dc00736f8b
Syahmi Zulkepli <msyahmizulkepli@gmail.com>
2022-05-24 12:49:45 +0800

    edited auto_clear_costmap_node

--- medibotv4/src/navigation/auto_clear_costmap_node.cpp
+++ medibotv4/src/navigation/auto_clear_costmap_node.cpp
@@ -9,10 +9,13 @@ int main(int argc, char **argv){
     std::string tf_prefix;
 
     nh_priv.param<std::string>("tf_prefix",tf_prefix,"");
+	if(tf_prefix != ""){
+		tf_prefix = "/"+tf_prefix;
+	}
 
 	ROS_INFO("Waiting for clear costmap service...");
-	ros::service::waitForService("/"+tf_prefix+"/move_base/clear_costmaps");
-	ros::ServiceClient clearClient = nh.serviceClient<std_srvs::Empty>("/"+tf_prefix+"/move_base/clear_costmaps");
+	ros::service::waitForService(tf_prefix+"/move_base/clear_costmaps");
+	ros::ServiceClient clearClient = nh.serviceClient<std_srvs::Empty>(tf_prefix+"/move_base/clear_costmaps");
     std_srvs::Empty srv;
     ROS_INFO("Clearing costmap periodically...");
 
