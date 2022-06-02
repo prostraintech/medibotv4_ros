174b1fb3241c25f2af1296b37496745e0ccea8bb
zulhafiz-zulkifli <zulh2605@gmail.com>
2022-06-03 04:12:39 +0800

    update waypoint control script

--- medibotv4/src/navigation/waypoint_control.py
+++ medibotv4/src/navigation/waypoint_control.py
@@ -6,47 +6,60 @@ import time
 import string
 from medibotv4.srv import *
 from move_base_msgs.msg import MoveBaseActionResult
-from actionlib_msgs.msg import GoalStatusArray
+from actionlib_msgs.msg import GoalStatusArray, GoalID
 
 class WaypointControl(object):
-    def __init__(self):
-        self._waypoints = ["spotA", "spotB", "spotC", "spotD"]
-        # _robot_status can be either moving or reached.
-        self._robot_status = "moving";
-        self._mb_result_sub = rospy.Subscriber('/move_base/result', MoveBaseActionResult , self.mb_result_sub_callback)
+    def __init__(self, waypoints=["spotA", "spotB", "spotC", "spotD"]):
+        self.waypoints = waypoints
+        self.robot_status = "moving"; # can be either moving or reached.
+        self.mb_result_sub = rospy.Subscriber('/move_base/result', MoveBaseActionResult , self.mb_result_sub_callback)
+        self.mb_cancel_pub = rospy.Publisher('/move_base/cancel', GoalID, queue_size=10)
+        self.Ntest = 1
+        self.start_time = 0.0
+        self.end_time = 0.0
+        rospy.on_shutdown(self.shutdownhook)
+        rospy.wait_for_service('/spots/send_goal') # wait for send goal service
 
     def mb_result_sub_callback(self, msg):
         if msg.status.status == 3: # if robot has reached goal
-            self._robot_status = "reached"
+            self.robot_status = "reached"
 
+    def shutdownhook(self):
+        msg = GoalID()
+        self.mb_cancel_pub.publish(msg)
+
+    def begin(self):
+        for i in self.waypoints:
+            send_goal_service_client = rospy.ServiceProxy('/spots/send_goal', SendGoal)
+            request = SendGoalRequest()
+            request.label = i
+            # check if going to first spot in the list
+            if i == self.waypoints[0]:
+                print("\n--------- TEST " + str(self.Ntest) + "---------")
+            # check if going to second spot in the list
+            if i == self.waypoints[1]:
+                self.start_time = rospy.get_time() # start the timer
+            # call service to send goal to a waypoint
+            response = send_goal_service_client(request)
+            print("\tMoving to " + i)
+            # wait until the goal is reached
+            while self.robot_status != "reached":
+                pass
+            # reached at a spot
+            print("\tReached at " + i + ".\n")
+            self.robot_status = "moving"
+            # check if reached at final spot in the list
+            if i == self.waypoints[-1]:
+                self.Ntest += 1
+                self.end_time = rospy.get_time() # end the timer
+                print("\tTime taken = " + str(round(self.end_time-self.start_time,2)) + " seconds")
 
 
 if __name__ == "__main__":
     rospy.init_node('waypoint_control_node', log_level=rospy.INFO) 
     wpc = WaypointControl()
-    Ncycle = 1
-    start_time = rospy.get_time()
-    rospy.wait_for_service('/spots/send_goal')
-    while not rospy.is_shutdown():
-        for label in wpc._waypoints:
-            # wait for send goal service
-            send_goal_service_client = rospy.ServiceProxy('/spots/send_goal', SendGoal)
-            msg = SendGoalRequest()
-            msg.label = label
-            # check if going to first spot
-            if label == wpc._waypoints[0]:
-                print("\n------- TEST " + str(Ncycle) + "-------")
-                start_time = rospy.get_time()
-            # call service to send goal to a waypoint
-            response = send_goal_service_client(msg)
-            print("Moving to " + label)
-            # is robot still moving? if yes wait
-            while wpc._robot_status != "reached":
-                pass
-            # reached at a spot
-            print("Reached at " + label + ".\n")
-            wpc._robot_status = "moving"
-            # check if reached at final spot
-            if label == wpc._waypoints[-1]:
-                Ncycle += 1
-                print("Time taken = " + str(round(rospy.get_time()-start_time,2)) + " seconds")
+    try:
+        while not rospy.is_shutdown():
+            wpc.begin()
+    except rospy.ROSInterruptException:
+        pass
