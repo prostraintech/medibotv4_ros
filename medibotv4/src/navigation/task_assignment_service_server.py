6f03ca2ca48fbb3382552cee5f92e33c9b9db7d8
Syahmi Zulkepli <msyahmizulkepli@gmail.com>
2022-06-02 22:50:44 +0800

    updated task assignment service server

--- medibotv4/src/navigation/task_assignment_service_server.py
+++ medibotv4/src/navigation/task_assignment_service_server.py
@@ -2,10 +2,12 @@
 import rospy
 import math
 from nav_msgs.srv import GetPlan, GetPlanRequest
+from std_msgs.msg import Bool
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import PoseStamped
 from std_srvs.srv import Trigger, TriggerResponse
 from move_base_msgs.msg import MoveBaseActionGoal
+from actionlib_msgs.msg import GoalStatusArray
 
 
 class TaskAssign(object):
@@ -14,12 +16,16 @@ class TaskAssign(object):
         self.robot1_start = PoseWithCovarianceStamped()
         self.robot2_start = PoseWithCovarianceStamped()
         self.robot_goal = PoseStamped()
+        self.robot1_status = "idle"
+        self.robot2_status = "idle"
 
         self.robot1_start_sub = rospy.Subscriber('/robot1/amcl_pose', PoseWithCovarianceStamped, self.robot1_start_sub_callback)
         self.robot2_start_sub = rospy.Subscriber('/robot2/amcl_pose', PoseWithCovarianceStamped, self.robot2_start_sub_callback)
         self.goal_sub = rospy.Subscriber('/nav_goal', PoseStamped, self.goal_sub_callback)
         self.robot1_goal_pub = rospy.Publisher("/robot1/move_base/goal", MoveBaseActionGoal, queue_size=10)
         self.robot2_goal_pub = rospy.Publisher("/robot2/move_base/goal", MoveBaseActionGoal, queue_size=10)
+        self.robot1_status_sub = rospy.Subscriber("/robot1/move_base/status", GoalStatusArray, self.robot1_status_sub_callback)
+        self.robot2_status_sub = rospy.Subscriber("/robot2/move_base/status", GoalStatusArray, self.robot2_status_sub_callback)
         
 
 
@@ -37,74 +43,154 @@ class TaskAssign(object):
     def goal_sub_callback(self, msg):
         self.robot_goal = msg
 
+    def robot1_status_sub_callback(self,msg):
+        i = 0
+        while i<len(msg.status_list):
+            if msg.status_list[i].status == 1:
+                self.robot1_status = "active"
+            else:
+                self.robot1_status = "idle"
+            i+=1
 
-    def task_assign_srv_callback(self, request):    
-        
-        response = TriggerResponse()
-        rospy.wait_for_service('/robot1/move_base/make_plan')
-        make_plan_service = rospy.ServiceProxy('/robot1/move_base/make_plan', GetPlan)
-        msg = GetPlanRequest()
-        msg.start.header = self.robot1_start.header
-        msg.start.pose = self.robot1_start.pose.pose
-        msg.goal = self.robot_goal
+    def robot2_status_sub_callback(self,msg):
+        i = 0
+        while i<len(msg.status_list):
+            if msg.status_list[i].status == 1:
+                self.robot2_status = "active"
+            else:
+                self.robot2_status = "idle"
+            i+=1
 
-        result = make_plan_service(msg)
 
-        first_time = True
-        prev_x = 0.0
-        prev_y = 0.0
-        total_distance_1 = 0.0
-        if len(result.plan.poses) > 0:
-            for current_point in result.plan.poses:
-                x = current_point.pose.position.x
-                y = current_point.pose.position.y
-                if not first_time:
-                    total_distance_1 += math.hypot(prev_x - x, prev_y - y) 
-                else:
-                    first_time = False
-                prev_x = x
-                prev_y = y
-        rospy.loginfo("robot1 Total Distance = "+str(total_distance_1)+" meters")
+    def task_assign_srv_callback(self, request):
+        response = TriggerResponse()    
+        if self.robot1_status == "idle" and self.robot2_status == "idle":
+            rospy.wait_for_service('/robot1/move_base/make_plan')
+            make_plan_service = rospy.ServiceProxy('/robot1/move_base/make_plan', GetPlan)
+            msg = GetPlanRequest()
+            msg.start.header = self.robot1_start.header
+            msg.start.pose = self.robot1_start.pose.pose
+            msg.goal = self.robot_goal
 
-        rospy.wait_for_service('/robot2/move_base/make_plan')
-        make_plan_service = rospy.ServiceProxy('/robot2/move_base/make_plan', GetPlan)
-        msg = GetPlanRequest()
-        msg.start.header = self.robot2_start.header
-        msg.start.pose = self.robot2_start.pose.pose
-        msg.goal = self.robot_goal
-        result = make_plan_service(msg)
+            result = make_plan_service(msg)
 
-        first_time = True
-        prev_x = 0.0
-        prev_y = 0.0
-        total_distance_2 = 0.0
-        if len(result.plan.poses) > 0:
-            for current_point in result.plan.poses:
-                x = current_point.pose.position.x
-                y = current_point.pose.position.y
-                if not first_time:
-                    total_distance_2 += math.hypot(prev_x - x, prev_y - y) 
-                else:
-                    first_time = False
-                prev_x = x
-                prev_y = y
-        rospy.loginfo("robot2 Total Distance = "+str(total_distance_2)+" meters")
-        Goal = MoveBaseActionGoal()
-        Goal.goal.target_pose.header.frame_id='map'
-        Goal.goal.target_pose.pose = self.robot_goal.pose
+            first_time = True
+            prev_x = 0.0
+            prev_y = 0.0
+            total_distance_1 = 0.0
+            if len(result.plan.poses) > 0:
+                for current_point in result.plan.poses:
+                    x = current_point.pose.position.x
+                    y = current_point.pose.position.y
+                    if not first_time:
+                        total_distance_1 += math.hypot(prev_x - x, prev_y - y) 
+                    else:
+                        first_time = False
+                    prev_x = x
+                    prev_y = y
+            rospy.loginfo("robot1 Total Distance = "+str(total_distance_1)+" meters")
 
-        if total_distance_1 > total_distance_2:
-            response.message = "robot2"
-            # self.robot2_goal_pub.publish(Goal)
-        else:
+            rospy.wait_for_service('/robot2/move_base/make_plan')
+            make_plan_service = rospy.ServiceProxy('/robot2/move_base/make_plan', GetPlan)
+            msg = GetPlanRequest()
+            msg.start.header = self.robot2_start.header
+            msg.start.pose = self.robot2_start.pose.pose
+            msg.goal = self.robot_goal
+            result = make_plan_service(msg)
+
+            first_time = True
+            prev_x = 0.0
+            prev_y = 0.0
+            total_distance_2 = 0.0
+            if len(result.plan.poses) > 0:
+                for current_point in result.plan.poses:
+                    x = current_point.pose.position.x
+                    y = current_point.pose.position.y
+                    if not first_time:
+                        total_distance_2 += math.hypot(prev_x - x, prev_y - y) 
+                    else:
+                        first_time = False
+                    prev_x = x
+                    prev_y = y
+            rospy.loginfo("robot2 Total Distance = "+str(total_distance_2)+" meters")
+            # Goal = MoveBaseActionGoal()
+            # Goal.goal.target_pose.header.frame_id='map'
+            # Goal.goal.target_pose.pose = self.robot_goal.pose
+
+            if total_distance_1 > total_distance_2:
+                response.message = "robot2"
+                # self.robot2_goal_pub.publish(Goal)
+            else:
+                response.message = "robot1"
+                # self.robot1_goal_pub.publish(Goal)
+
+            response.success = True
+            return response
+        elif self.robot1_status == "idle" and self.robot2_status == "active":
+            rospy.loginfo("robot2 is busy... assigning task to robot1")
+            rospy.wait_for_service('/robot1/move_base/make_plan')
+            make_plan_service = rospy.ServiceProxy('/robot1/move_base/make_plan', GetPlan)
+            msg = GetPlanRequest()
+            msg.start.header = self.robot1_start.header
+            msg.start.pose = self.robot1_start.pose.pose
+            msg.goal = self.robot_goal
+
+            result = make_plan_service(msg)
+
+            first_time = True
+            prev_x = 0.0
+            prev_y = 0.0
+            total_distance_1 = 0.0
+            if len(result.plan.poses) > 0:
+                for current_point in result.plan.poses:
+                    x = current_point.pose.position.x
+                    y = current_point.pose.position.y
+                    if not first_time:
+                        total_distance_1 += math.hypot(prev_x - x, prev_y - y) 
+                    else:
+                        first_time = False
+                    prev_x = x
+                    prev_y = y
+            rospy.loginfo("robot1 Total Distance = "+str(total_distance_1)+" meters")
             response.message = "robot1"
-            # self.robot1_goal_pub.publish(Goal)
+            response.success = True
+            return response
+        elif self.robot1_status == "active" and self.robot2_status == "idle":
+            rospy.loginfo("robot1 is busy... assigning task to robot2")
+            rospy.wait_for_service('/robot2/move_base/make_plan')
+            make_plan_service = rospy.ServiceProxy('/robot2/move_base/make_plan', GetPlan)
+            msg = GetPlanRequest()
+            msg.start.header = self.robot2_start.header
+            msg.start.pose = self.robot2_start.pose.pose
+            msg.goal = self.robot_goal
+            result = make_plan_service(msg)
 
-        response.success = True
-        return response
+            first_time = True
+            prev_x = 0.0
+            prev_y = 0.0
+            total_distance_2 = 0.0
+            if len(result.plan.poses) > 0:
+                for current_point in result.plan.poses:
+                    x = current_point.pose.position.x
+                    y = current_point.pose.position.y
+                    if not first_time:
+                        total_distance_2 += math.hypot(prev_x - x, prev_y - y) 
+                    else:
+                        first_time = False
+                    prev_x = x
+                    prev_y = y
+            rospy.loginfo("robot2 Total Distance = "+str(total_distance_2)+" meters")
+            response.message = "robot2"
+            response.success = True
+            return response
+        else:
+            rospy.loginfo("both robots are busy... please wait for robot to complete task or cancel robot task manually")
+            response.message = "none"
+            response.success = True
+            return response
 
    
 if __name__ == "__main__":
-    rospy.init_node('Get_plan_service_server', log_level=rospy.INFO) 
+    rospy.init_node('task_assignment_service_server', log_level=rospy.INFO) 
     task_object = TaskAssign()
     rospy.spin() # mantain the service open.
