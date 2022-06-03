7c564213f4a5b5f2333da8efa967bb23a1c21886
zulhafiz-zulkifli <zulh2605@gmail.com>
2022-06-03 03:13:57 -0700

    calculate distance

---
+++ medibotv4/src/navigation/calculate_distance_traveled.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+import math
+import roslib, rospy
+from move_base_msgs.msg import MoveBaseActionGoal, MoveBaseActionFeedback
+
+class CalculateDistanceTraveled(object):
+    def __init__(self):
+        self.total_distance = 0
+        self.prev_x = 0.0
+        self.prev_y = 0.0
+        self.goal_point = []
+        self.first_time = True
+        self.pose_subscriber = rospy.Subscriber("/move_base/feedback", MoveBaseActionFeedback, self.addPointToTotalDistance)
+        self.goal_subscriber = rospy.Subscriber("/move_base/goal", MoveBaseActionGoal, self.getGoalPoint)
+
+    def distancePoints(self,x1,y1,x2,y2):
+        return math.hypot(x2 - x1, y2 - y1)
+
+    def getGoalPoint(self,g):
+        self.goal_point = g
+
+    def addPointToTotalDistance(self,current_point):
+        if self.first_time:
+            self.first_time = False
+            self.prev_x = current_point.feedback.base_position.pose.position.x
+            self.prev_y = current_point.feedback.base_position.pose.position.y
+        else:
+            x = current_point.feedback.base_position.pose.position.x
+            y = current_point.feedback.base_position.pose.position.y
+
+            self.total_distance += self.distancePoints(x, y, self.prev_x, self.prev_y)
+
+            self.prev_x = x
+            self.prev_y = y
+            #print("Current distance traveled= " + str(self.total_distance) + " meters")
+            if self.goal_point != []:
+                if self.distancePoints(x, y, self.goal_point.goal.target_pose.pose.position.x, self.goal_point.goal.target_pose.pose.position.y) <= 0.1:
+                    self.pose_subscriber.unregister()
+                    self.goal_subscriber.unregister()
+                    #print("Total Distance = " + str(self.total_distance) + " meters")
+
+    def getTotalDistance(self):
+        return self.total_distance
--- medibotv4/src/navigation/waypoint_control.py
+++ medibotv4/src/navigation/waypoint_control.py
@@ -7,9 +7,11 @@ import string
 from medibotv4.srv import *
 from move_base_msgs.msg import MoveBaseActionResult
 from actionlib_msgs.msg import GoalStatusArray, GoalID
+from calculate_distance_traveled import CalculateDistanceTraveled
+
 
 class WaypointControl(object):
-    def __init__(self, waypoints=["spotA", "spotB", "spotC", "spotD"]):
+    def __init__(self, waypoints=["spotA", "spotB","spotC", "spotD"]):
         self.waypoints = waypoints
         self.robot_status = "moving"; # can be either moving or reached.
         self.mb_result_sub = rospy.Subscriber('/move_base/result', MoveBaseActionResult , self.mb_result_sub_callback)
@@ -17,6 +19,8 @@ class WaypointControl(object):
         self.Ntest = 1
         self.start_time = 0.0
         self.end_time = 0.0
+        self.total_distance = 0.0
+        #cdt = None
         rospy.on_shutdown(self.shutdownhook)
         rospy.wait_for_service('/spots/send_goal') # wait for send goal service
 
@@ -30,29 +34,35 @@ class WaypointControl(object):
 
     def begin(self):
         for i in self.waypoints:
+            cdt = CalculateDistanceTraveled() # start calculating distance traveled
             send_goal_service_client = rospy.ServiceProxy('/spots/send_goal', SendGoal)
             request = SendGoalRequest()
             request.label = i
             # check if going to first spot in the list
             if i == self.waypoints[0]:
                 print("\n--------- TEST " + str(self.Ntest) + "---------")
-            # check if going to second spot in the list
-            if i == self.waypoints[1]:
-                self.start_time = rospy.get_time() # start the timer
             # call service to send goal to a waypoint
             response = send_goal_service_client(request)
-            print("\tMoving to " + i)
+            print("  Moving to " + i)
             # wait until the goal is reached
             while self.robot_status != "reached":
                 pass
             # reached at a spot
-            print("\tReached at " + i + ".\n")
+            print("  Reached at " + i + ".\n")
             self.robot_status = "moving"
+            # check if reached at first spot in the list
+            if i == self.waypoints[0]:
+                self.start_time = rospy.get_time() # start the timer
+            else:
+                self.total_distance += cdt.getTotalDistance()
             # check if reached at final spot in the list
             if i == self.waypoints[-1]:
                 self.Ntest += 1
                 self.end_time = rospy.get_time() # end the timer
-                print("\tTime taken = " + str(round(self.end_time-self.start_time,2)) + " seconds")
+                print("  Time taken = " + str(round(self.end_time-self.start_time,2)) + " seconds")
+                print("  Total Distance Traveled = " + str(self.total_distance) + " meters")
+                self.total_distance = 0.0
+
 
 
 if __name__ == "__main__":
