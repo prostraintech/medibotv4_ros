9b2401a15d659fd8e8e7b6953bf3dea087bf21f9
Syahmi Zulkepli <msyahmizulkepli@gmail.com>
2022-06-06 12:14:14 +0800

    updated robot1.launch to include ekf_localization

--- medibotv4/src/navigation/clear_costmap_node.py
+++ medibotv4/src/navigation/clear_costmap_node.py
@@ -8,13 +8,16 @@ from move_base_msgs.msg import MoveBaseActionGoal
 from std_srvs.srv import Empty, EmptyRequest
 
 
-def sub_callback(msg):
-    clear_costmap_service_client = rospy.ServiceProxy('/move_base/clear_costmaps', Empty)
+def sub_callback(robot_namespace):
+    clear_costmap_service_client = rospy.ServiceProxy(robot_namespace+'/move_base/clear_costmaps', Empty)
     clear_costmap_service_request = EmptyRequest()
     clear_costmap_service_response = clear_costmap_service_client(clear_costmap_service_request)
 
 
 if __name__ == "__main__":
-    rospy.init_node('clear_costmap_node', log_level=rospy.INFO) 
-    _ = rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, sub_callback)
+    rospy.init_node('clear_costmap_node', log_level=rospy.INFO)
+    robot_namespace = rospy.get_param("~robot_ns", "")
+    if robot_namespace != "":
+        robot_namespace = "/" + robot_namespace 
+    _ = rospy.Subscriber(robot_namespace+'/move_base/goal', MoveBaseActionGoal, sub_callback(robot_namespace))
     rospy.spin() # mantain the service open.
