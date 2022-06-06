4715d5b7d3006245c3bc63b6c124055897ccb3f4
Syahmi Zulkepli <msyahmizulkepli@gmail.com>
2022-06-06 13:21:43 +0800

    updated clear_costmap_node

--- medibotv4/src/navigation/clear_costmap_node.py
+++ medibotv4/src/navigation/clear_costmap_node.py
@@ -7,17 +7,17 @@ import string
 from move_base_msgs.msg import MoveBaseActionGoal
 from std_srvs.srv import Empty, EmptyRequest
 
+robot_namespace = rospy.get_param("~robot_ns", "")
+if robot_namespace != "":
+    robot_namespace = "/" + robot_namespace 
 
-def sub_callback(robot_namespace):
+def sub_callback(msg):
     clear_costmap_service_client = rospy.ServiceProxy(robot_namespace+'/move_base/clear_costmaps', Empty)
     clear_costmap_service_request = EmptyRequest()
     clear_costmap_service_response = clear_costmap_service_client(clear_costmap_service_request)
 
 
 if __name__ == "__main__":
-    rospy.init_node('clear_costmap_node', log_level=rospy.INFO)
-    robot_namespace = rospy.get_param("~robot_ns", "")
-    if robot_namespace != "":
-        robot_namespace = "/" + robot_namespace 
+    rospy.init_node('clear_costmap_node', log_level=rospy.INFO)    
     _ = rospy.Subscriber(robot_namespace+'/move_base/goal', MoveBaseActionGoal, sub_callback(robot_namespace))
     rospy.spin() # mantain the service open.
